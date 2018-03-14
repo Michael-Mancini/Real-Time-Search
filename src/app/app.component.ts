24e5f00e3146953f8b621ad465e1ecf50cc7c25f
Michael Mancini <michaelmancinidev@gmail.com>
2018-03-14 06:32:14 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { SearchService } from './search.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SearchService]
 })
 export class AppComponent {
-  title = 'app';
+  // changed results type from Object to any to avoid bogus typescript error
+  results: any;
+  searchTerm$ = new Subject<string>();
+
+  constructor(private searchService: SearchService) {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        this.results = results;
+        this.results = this.results.results;
+        // only doing this funky declaration to avoid bogus ts error
+      });
+  }
 }
